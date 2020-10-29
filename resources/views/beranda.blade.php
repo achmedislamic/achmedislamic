cb045f12652712c540d14b72471014a59686abf0
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2020-10-29 21:13:17 +0800

    project, site technology done

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -30,33 +30,50 @@
         </div>
     </div>
 
+    <div class="container-fluid my-5">
+        <div class="row">
+            <div class="col text-center">
+                <img src="/img/responsif.png" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
     <div class="container my-5">
         <div class="row">
             <div class="col">
-                <h2>Berikut beberapa proyek yang pernah saya kerjakan</h2>
+                <h2 class="mb-4">Berikut beberapa proyek yang pernah saya kerjakan</h2>
                     <ul class="list-unstyled">
                         <li class="media">
-                        <img src="..." class="mr-3" alt="...">
-                        <div class="media-body">
-                            <h5 class="mt-0 mb-1">List-based media object</h5>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
+                            <svg class="bi text-primary mr-3" width="32" height="32">
+                                <use xlink:href="/icons/bootstrap-icons.svg#check-circle-fill"/>
+                            </svg>
+                            <div class="media-body">
+                                <h5 class="mt-0 mb-1">eNTeBePlan</h5>
+                                <p>Merupakan sistem perencanaan APBD untuk Provinsi NTB.</p>
+                            </div>
                         </li>
-                    <li class="media my-4">
-                      <img src="..." class="mr-3" alt="...">
-                      <div class="media-body">
-                        <h5 class="mt-0 mb-1">List-based media object</h5>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                      </div>
-                    </li>
-                    <li class="media">
-                      <img src="..." class="mr-3" alt="...">
-                      <div class="media-body">
-                        <h5 class="mt-0 mb-1">List-based media object</h5>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                      </div>
-                    </li>
                   </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container my-5">
+        <div class="row">
+            <div class="col">
+                <h2>Eiittsss! Ingin tahu situs ini menggunakan teknologi web apa saja?</h2>
+                <p>Ini dia teknologi yang dipakai di situs pribadi saya ini.</p>
+                <ol>
+                    <li>Laravel sebagai Backend</li>
+                    <li>Bootstrap dan jQuery untuk Frontend</li>
+                    <li>Livewire untuk form yang dinamis</li>
+                    <li>Bootstrap Icons</li>
+                    <li>PHP versi 7.4 terbaru</li>
+                    <li>Apache untuk web server</li>
+                </ol>
+                <p>Bahkan, saya dengan senang hati berbagi source code untuk situs ini. 
+                    Anda bisa download atau clone repositorynya 
+                    <a target="_blank" href="https://github.com/achmedislamic/achmedislamic">di Github saya.</a>
+                </p>
             </div>
         </div>
     </div>
