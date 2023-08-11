5be644241881f44aec2ffdb8f84d02bc96cfce70
achmedislamic <achmedislamic@outlook.com>
2023-08-11 08:38:57 +0800

    wip

--- resources/views/livewire/pembuatan-situs.blade.php
+++ resources/views/livewire/pembuatan-situs.blade.php
@@ -61,7 +61,7 @@
         <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Buat Situs Web Murah & Berkualitas!</h1>
             <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl/10 sm:px-16 lg:px-48">
-                Sudah saatnya Anda memiliki situs sendiri untuk memperkenalkan diri Anda maupun bisnis yang Anda miliki!
+                Perkenalkan diri atau bisnis Anda ke seluruh dunia dengan situs web hari ini juga!
             </p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
