7d2539e326d72935da1b187e70cb6cd0dc7cf896
Achmed Islamic Hernawan <achmedislamic@outlook.com>
2023-08-14 15:36:40 +0800

    update harga

--- resources/views/livewire/pembuatan-situs.blade.php
+++ resources/views/livewire/pembuatan-situs.blade.php
@@ -249,7 +249,7 @@
                         <svg class="w-5 h-5 mt-1 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                         </svg>
-                        <p>Tempat hosting terpercaya dengan <span class="font-bold">biaya mulai dari Rp. 26.400 per bulan.</span></p>
+                        <p>Tempat hosting terpercaya dengan <span class="font-bold">biaya mulai dari Rp. 16.500 per bulan.</span></p>
                     </li>
                     <li class="flex items-start text-md">
                         <svg class="w-5 h-5 mt-1 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
